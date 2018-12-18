88b2f7dcd48d12253063f9515c5c65691c518c14
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-12-18 12:27:23 +0100

    commit

--- sql/MediaTv2_mysql_create.sql
+++ sql/MediaTv2_mysql_create.sql
@@ -1,23 +1,23 @@
 CREATE TABLE `Artikel` (
-	`article_id` int NOT NULL AUTO_INCREMENT,
+	`article_id` int(255) NOT NULL AUTO_INCREMENT,
 	`article_text` TEXT NOT NULL,
 	`author` varchar NOT NULL,
 	PRIMARY KEY (`article_id`)
 );
 
 CREATE TABLE `Video` (
-	`video_id` int NOT NULL AUTO_INCREMENT,
-	`lengthInSeconds` INT NOT NULL,
+	`video_id` int(255) NOT NULL AUTO_INCREMENT,
+	`lengthInSeconds` INT(255) NOT NULL,
 	`resolution` varchar NOT NULL,
 	`photographer` varchar NOT NULL,
 	PRIMARY KEY (`video_id`)
 );
 
 CREATE TABLE `Billede` (
-	`billede_id` int NOT NULL AUTO_INCREMENT,
+	`billede_id` int(255) NOT NULL AUTO_INCREMENT,
 	`file_type` varchar NOT NULL,
-	`width` int NOT NULL,
-	`height` int NOT NULL,
+	`width` int(255) NOT NULL,
+	`height` int(255) NOT NULL,
 	`photographer` varchar NOT NULL,
 	PRIMARY KEY (`billede_id`)
 );
